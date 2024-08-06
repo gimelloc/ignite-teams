3504da27bcc08d1bdbc6507390f9d165745a1235
Gislain <gislainemcasa@gmail.com>
2024-08-06 19:40:43 -0300

    ajustes expo

--- src/components/GroupCard/index.tsx
+++ src/components/GroupCard/index.tsx
@@ -1,5 +1,5 @@
 import { TouchableOpacityProps } from "react-native";
-import { Container, Title } from "./styles";
+import { Container, Icon, Title } from "./styles";
 
 type Props = TouchableOpacityProps & {
     title: string;
@@ -8,6 +8,7 @@ type Props = TouchableOpacityProps & {
 export function GroupCard({ title, ...rest }: Props){
     return (
         <Container {...rest}>
+            <Icon />
             <Title>
                 {title}
             </Title>
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,15 +2,27 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import Highlight from '@components/Highligth';
 import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Rocketseat', 'Amigos']);
   return (
     <Container>
       <Header showBackButton />
 
       <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
 
-      <GroupCard title='Galera do ignite' />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
+      />
     </Container>
   );
 }
