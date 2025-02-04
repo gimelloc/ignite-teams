e9415e448794b9800b526050225c917554fc2d72
gimelloc <gislainemcasa@gmail.com>
2025-02-04 11:32:47 -0300

    fix dependencies

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
 import { BackButton, BackIcon, Container, Logo } from "./styles";
-import logoImg from '@assets/logo.png';
-import { CaretLeft } from "phosphor-react-native";
+import logoImg from "@assets/logo.png";
 
 type Props = {
-    showBackButton?: boolean
-}
+  showBackButton?: boolean;
+};
 
-export function Header({ showBackButton = false }: Props){
-    return (
-        <Container>
-            {showBackButton && <BackButton>
-                <BackIcon />
-            </BackButton>}
-            <Logo source={require('../../assets/logo.png')} />
-        </Container>
-    )
+export function Header({ showBackButton = false }: Props) {
+  return (
+    <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+      <Logo source={logoImg} />
+    </Container>
+  );
 }
